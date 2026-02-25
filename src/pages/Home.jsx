c884c23260321a42f88dff320004a1ac530af1f8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-25 21:55:47 +0600

    add pages and images and components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroCarousel from "../components/Carousel/HeroCarousel";
+import Features from "../components/Features";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroCarousel />
+      <Features />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router";
+import About from "../pages/About";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* <Route index element={<Home />}></Route>
+      <Route path="about" element={<About />} /> */}
+
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
